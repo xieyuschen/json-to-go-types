6744f472b8f1caa60efa7ebadb6497dece1d0e0b
Xieyuschen <xieyuschen@gmail.com>
2020-12-08 15:21:02 +0800

    refresh go mod

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
-	github.com/jessevdk/go-flags v1.4.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"fmt"
 	flags "github.com/jessevdk/go-flags"
+	"os"
 )
 //func ReadSettingsFromFile(settingFilePath string)(config models.Config){
 //	jsonFile, err := os.Open(settingFilePath)
@@ -17,7 +18,7 @@ import (
 //	}
 //	return config
 //}
-type Opts struct {
+var opts struct {
 
 	Generator	 bool `short:"g"`
 	Version bool `short:"v" long:"version"`
@@ -28,13 +29,12 @@ func main(){
 	//generator.GenerateModels("config.json")
 	//config:=ReadSettingsFromFile("config.json")
 	//fmt.Println(config.Config1.C12)
-	var opts Opts
 
-	parser := flags.NewParser(&opts, flags.Default)
-	parser.Name = "gocloc"
-	parser.Usage = "[OPTIONS] PATH[...]"
+	_, err := flags.ParseArgs(&opts, os.Args)
 
-
+	if err != nil {
+		panic(err)
+	}
 	if opts.Version{
 		fmt.Println("v0.1")
 	}else if opts.Generator{
@@ -42,12 +42,12 @@ func main(){
 		var jsonPath string
 		var storage string
 		if opts.ConfigPath!=""{
-			jsonPath= opts.ConfigPath
+			jsonPath=opts.ConfigPath
 		}else {
 			jsonPath="config.json"
 		}
 		if opts.StoragePath!=""{
-			storage= opts.StoragePath
+			storage=opts.StoragePath
 		}else {
 			storage="./models/Config_gen.go"
 		}
